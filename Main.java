2f54eaa0c3c08450a0616504cad1b1ad12768b4f
MargoSheleg <122802451+MargoSheleg@users.noreply.github.com>
2023-01-25 23:04:15 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("Он ");
+        System.out.print("металлическую ");
+        System.out.println("птицу");
+        System.out.print("Поднимает ");
+        System.out.print("в ");
+        System.out.println("облака.");
+        System.out.print("Теперь ");
+        System.out.print("воздушная ");
+        System.out.println("граница");
+        System.out.print("Надёжна ");
+        System.out.print("и ");
+        System.out.print("крепка!");
+    }
+}
